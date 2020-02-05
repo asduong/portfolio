052b043319068dc922966da2f6847b0165bf5b40
Andrew Duong <duongandrews@gmail.com>
2020-02-04 21:35:57 -0500

    Completed Home Page

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./CustomNavbar.css";
 
 export default class CustomNavbar extends React.Component {
   render() {
     return (
       <Navbar default collapseOnSelect>
-        <Navbar.Brand>
-          <Link to="/">AD</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-
+        <Navbar>
+          <Navbar.Brand>
+            <Link to="/">Home</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem eventKey={1} componentClass={Link} to="/">
-              Home
-            </NavItem>
-            <NavItem eventKey={2} componentClass={Link} to="/about">
+            <NavItem eventKey={1} componentClass={Link} to="/about">
               About
             </NavItem>
-            <NavItem eventKey={3} componentClass={Link} to="/projects">
+            <NavItem eventKey={2} componentClass={Link} to="/projects">
               Projects
             </NavItem>
           </Nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Jumbotron, Container, Row, Col, Image, Button } from "react-bootstrap";
 import "./Home.css";
+import { ReactComponent as Icon } from "../icons/cat.svg";
+import { ReactComponent as Mail } from "../icons/mail.svg";
+import { ReactComponent as LinkedIn } from "../icons/linkedin.svg";
 
 class Home extends React.Component {
   render() {
     return (
-      <Container>
-        <Jumbotron>
-          <h2>Andrew Duong</h2>
-          <p>Full Stack Developer</p>
-        </Jumbotron>
-        <Link to="/about">
-          <Button bsStyle="primary">About</Button>
-        </Link>
-      </Container>
+      <div className="content">
+        <h1>Andrew Duong</h1>
+        <h5>Full Stack Developer</h5>
+        <div>
+          <a href="https://github.com/asduong">
+            <Icon height={25} width={40} fill={"#EAEAEA"}></Icon>
+          </a>
+          <a href="mailto:duongandrews@gmail.com">
+            <Mail height={25} width={40} fill={"#EAEAEA"}></Mail>
+          </a>
+          <a href="https://www.linkedin.com/in/duong-andrew/">
+            <LinkedIn height={25} width={40} fill={"#EAEAEA"}></LinkedIn>
+          </a>
+        </div>
+      </div>
     );
   }
 }
